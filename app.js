e72f836961713e2a76ddcb5ff8a170e5a181263b
Anirudh <developeranirudhgupta@gmail.com>
2022-01-06 16:55:45 +0530

    initial

---
+++ app.js
@@ -0,0 +1,22 @@
+const express=require('express');
+const app=express();
+const port=process.env.PORT||8000;
+const path=require('path');
+const { start } = require('repl');
+app.set('view engine', 'ejs');
+app.set('views',path.join(__dirname,'/views'));
+
+
+
+
+
+
+app.get('/',(req,res)=>
+{
+    res.render('home');
+})
+///set template engine
+app.listen(port,()=>
+{
+    console.log(`listening on ${port}`);
+}) 
